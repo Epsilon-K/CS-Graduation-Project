d8b2a4bd79ac30f319b43bafddd6084e5d89ab05
Epsilon <dev.djvan@gmail.com>
2020-11-03 22:14:48 +0200

    Presentation

--- Python CV program/MT_VisionUI/mt_visionui.cpp
+++ Python CV program/MT_VisionUI/mt_visionui.cpp
@@ -12,3 +12,41 @@ MT_VisionUI::~MT_VisionUI()
 {
     delete ui;
 }
+
+void MT_VisionUI::on_saveBtn_clicked()
+{
+    QFile file("settings.txt");
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+        qDebug() << "Error, Can't Open File!";
+        return;
+    }
+
+    QTextStream out(&file);
+    out << "camera device id: " << ui->devIdSB->value() << "\n";
+    out << "client id: " << ui->clientIdSB->value() << "\n";
+    out << "roi left: " << ui->roiLSB->value() << "\n";
+    out << "roi right: " << ui->roiRSB->value() << "\n";
+    out << "roi top: " << ui->roiTSB->value() << "\n";
+    out << "roi bottom: " << ui->roiBSB->value() << "\n";
+    out << "server url: " << ui->serverUrlLineEdit->text() << "\n";
+    out << "server update: " << ui->serverUpdateSB->value() << "\n";
+    out << "send to server: " << (ui->sendCB->isChecked() ? "true" : "false") << "\n";
+    out << "random id: " << (ui->randomIdCB->isChecked() ? "true" : "false") << "\n";
+    out << "random start: " << ui->randBeginSB->value() << "\n";
+    out << "random end: " << ui->randEndSB->value() << "\n";
+    out << "use video: " << (ui->useVideoCB->isChecked() ? "true" : "false") << "\n";
+    out << "video path: " << ui->videoNameLineEdit->text();
+}
+
+void MT_VisionUI::on_runBtn_clicked()
+{
+    QString currPath = QDir::currentPath();
+
+}
+
+
+
+
+
+
+
--- Python CV program/MT_VisionUI/mt_visionui.h
+++ Python CV program/MT_VisionUI/mt_visionui.h
@@ -2,6 +2,10 @@
 #define MT_VISIONUI_H
 
 #include <QMainWindow>
+#include <QDebug>
+#include <QFile>
+#include <QTextStream>
+#include <QDir>
 
 namespace Ui {
 class MT_VisionUI;
@@ -14,6 +18,11 @@ class MT_VisionUI : public QMainWindow
 public:
     explicit MT_VisionUI(QWidget *parent = nullptr);
     ~MT_VisionUI();
+
+private slots:
+    void on_saveBtn_clicked();
+
+    void on_runBtn_clicked();
 
 private:
     Ui::MT_VisionUI *ui;
